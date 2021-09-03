40557050daf9f95cb117f22b6bbe2f314497d5aa
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-03 12:23:56 +0530

    Employ Daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,15 +3,19 @@ package com.bl.java;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("WELL COME TO EMPLOYEE WAGE COMPUTATION");
+		int IS_PRESENT=1;
+        int WAGE_PER_HOUR=20;
+        int empWage=0;
+        double empCheck=Math.floor(Math.random()*10)%2; //to randomly generate num. 0 or 1
 
-		int PRESENT = 1;
-		int ABSENT = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == PRESENT)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+
+        if(empCheck==IS_PRESENT) {
+            int empHrs = 8;
+            empWage = WAGE_PER_HOUR * empHrs;
+        }
+        else
+            empWage=0;
+        System.out.println("Employee daily wage is :"+empWage);
 	}
 
 }
