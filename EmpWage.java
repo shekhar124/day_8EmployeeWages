2655b8adabd75dd87eabd3b26c225ccb03b4b13b
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-03 12:27:51 +0530

    Part time emp wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,19 +3,21 @@ package com.bl.java;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		int IS_PRESENT=1;
-        int WAGE_PER_HOUR=20;
-        int empWage=0;
-        double empCheck=Math.floor(Math.random()*10)%2; //to randomly generate num. 0 or 1
+		 int IS_PRESENT_FULL_TIME=1;
+	        int IS_PRESENT_HALF_TIME=2;
+	        int WAGE_PER_HOUR=20;
+	        int empWorkHrs=0;
+	        int empWage=0;
+	        double empCheck=Math.floor(Math.random()*10)%3; //to randomly generate num. 0 or 1
 
-
-        if(empCheck==IS_PRESENT) {
-            int empHrs = 8;
-            empWage = WAGE_PER_HOUR * empHrs;
-        }
-        else
-            empWage=0;
-        System.out.println("Employee daily wage is :"+empWage);
+	        if(empCheck==IS_PRESENT_FULL_TIME)
+	            empWorkHrs=8;
+	        else if(empCheck==IS_PRESENT_HALF_TIME)
+	            empWorkHrs=4;
+	        else
+	            empWorkHrs=0;
+	        empWage=WAGE_PER_HOUR * empWorkHrs;
+	        System.out.println("Employee's day wage is :"+empWage);
 	}
 
 }
